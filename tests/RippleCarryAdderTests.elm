377c64b94101cd6cc65ffea041930dd473f4521c
git <tran1619@gmail.com>
2020-05-16 13:00:48 -0400

    add more tests

--- tests/RippleCarryAdderTests.elm
+++ tests/RippleCarryAdderTests.elm
@@ -1,4 +1,4 @@
-module RippleCarryAdderTests exposing (andGateTests, fullAdderTests, inverterTests, orGateTests)
+module RippleCarryAdderTests exposing (andGateTests, fullAdderTests, halfAdderTests, inverterTests, orGateTests, rippleCarryAdderTests)
 
 import RippleCarryAdder exposing (..)
 import Test exposing (..)
@@ -58,6 +58,26 @@ orGateTests =
                     |> Expect.equal 1
         ]
 
+halfAdderTests =
+    describe "Half Adder"
+        [ test "sum and carry-out are 0 when both inputs are 0" <|
+            \_ ->
+                halfAdder 0 0
+                    |> Expect.equal { carry = 0, sum = 0}
+        , test "sum is 1 and carry-out is 0 when first input is 0, second input is 1" <|
+            \_ ->
+                halfAdder 0 1
+                    |> Expect.equal { carry = 0, sum = 1}
+        , test "sum is 1 and carry-out is 0 when first input is 1, second input is 0" <|
+            \_ ->
+                halfAdder 1 0
+                    |> Expect.equal { carry = 0, sum = 1}
+        , test "sum is 0 and carry-out is 1 when both inputs are 1" <|
+            \_ ->
+                halfAdder 1 1
+                    |> Expect.equal { carry = 1, sum = 0 }
+        ]       
+
 fullAdderTests =
     describe "Full adder"        
         [ test "sum and carry-out are 0 when both inputs and carry-in are 0" <|
@@ -93,3 +113,33 @@ fullAdderTests =
                 fullAdder 1 1 1 
                     |> Expect.equal {carry = 1, sum = 1}
         ]
+
+rippleCarryAdderTests =
+    describe "4-bit ripple carry adder"
+        [ describe "given two binary numbers and a carry-in digit"
+            [ test "returns the sum of those numbers and a carry-out digit" <|
+                \_ ->
+                    rippleCarryAdder 1001 1101 1
+                        |> Expect.equal 10111
+            ]
+        , describe "when the ist input is 1111, and the 2nd input is 1111"
+            [ test "and carry-in is 0, the output is 11110" <|
+                \_ ->
+                    rippleCarryAdder 1111 1111 0
+                        |> Expect.equal 11110
+            , test  "and carry-in is 1, the output is 11111" <|
+                \_ ->
+                    rippleCarryAdder 1111 1111 1
+                        |> Expect.equal 11111
+            ]
+        , describe "when the first input is 0000, and the 2nd input is 0000"
+            [ test "and carry-in is 0, the output is 0000" <|
+                \_ ->
+                    rippleCarryAdder 0 0 0
+                        |> Expect.equal 0
+            , test "and carry-in is 1, the output is 0001" <|
+                \_ ->
+                    rippleCarryAdder 0 0 1
+                        |> Expect.equal 1
+            ]
+        ]        
