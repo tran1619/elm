f9d32f390d3d5fab480f05a43aa40d6453f4a1bb
Michael Tran <tran1619@gmail.com>
2020-05-25 22:03:00 -0400

    Adding increment & decrement by 5

--- src/Counter.elm
+++ src/Counter.elm
@@ -13,24 +13,30 @@ main =
         , update = update
         }
 
-type Msg = Increment | Decrement
+type Msg = Increment5 | Increment | Decrement | Decrement5
 
 
 -- Update
 update msg model =
     case msg of
+        Increment5 ->
+            model + 5
         Increment ->
             model + 1
 
         Decrement ->
             model - 1
+        Decrement5 ->
+            model - 5
 
 
 -- View
 view model =
     div []
-        [ button [ onClick Decrement] [ text "-"]
+        [ button [ onClick Decrement5 ] [ text "-5"]
+        , button [ onClick Decrement] [ text "-"]
         , div [] [ text (String.fromInt model) ]
         , button [ onClick Increment] [ text "+"]
+        , button [ onClick Increment5] [ text "+5"]
         ]            
     
